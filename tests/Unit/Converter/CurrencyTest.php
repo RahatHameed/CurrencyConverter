e1d3c4aad2ffc2b11e3e1834c694b757a79df31d
Rahat hameed <the_rahat_online@yahoo.com>
2023-06-01 13:30:29 +0200

    Add tests for Exception

--- tests/Unit/Converter/CurrencyTest.php
+++ tests/Unit/Converter/CurrencyTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Converter;
 use App\Converter\Currency;
 use App\InputData\InputDataInterface;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class CurrencyTest extends TestCase
 {
@@ -28,6 +29,25 @@ class CurrencyTest extends TestCase
         $currency = new Currency($inputDataInterface);
         $this->assertEquals($expected['exchangeRates'], $currency->getExchangeRates());
         $this->assertEquals($expected['baseCurrency'], $currency->getBaseCurrency());
+    }
+
+    public function testCurrencyClassException(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $expected = [
+            "invalidExchangeRates" => [
+                "EUR" => 1,
+                "USD" => 1.0857,
+                "CHF" => 0.98,
+                "CNY" => 7.61
+            ]
+        ];
+
+        $inputDataInterface = $this->createMock(InputDataInterface::class);
+        $inputDataInterface
+            ->method('getData')
+            ->willReturn($expected);
+        new Currency($inputDataInterface);
     }
 
 }
